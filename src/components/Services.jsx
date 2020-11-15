2af7fb5f3ae99846d55df28525073ca2cb046007
unknown <DURODOLA-ABDULKABIR>
2020-11-14 22:14:52 -0800

    fixed a typo

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -5,7 +5,7 @@ function Services(){
         <div className="services">
             <h2>My services</h2>
             <hr className="line" />
-            <p><strong>Design :</strong> I design simple, clean content structures and patterns that give users awsome experiences.</p>
+            <p><strong>Design :</strong> I design simple, clean content structures and patterns that give users awesome experiences.</p>
             <p><strong>Development :</strong> I develop designs to precision and derive much satisfaction from bringing ideas to live.</p>
         </div>  
     )
